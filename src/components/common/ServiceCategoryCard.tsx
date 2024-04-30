36fb2c85086c868f91dc6b676ff6dfd33f3a9007
Mahadi92 <mahadi.hasan.937524.mh@gmail.com>
2024-04-30 16:18:17 +0600

    some changes

--- src/components/common/ServiceCategoryCard.tsx
+++ src/components/common/ServiceCategoryCard.tsx
@@ -1,20 +1,41 @@
+import { services } from '@/data/services';
+import Link from 'next/link';
 import React from 'react';
 
 interface PropsType {
   item: {
     icon: JSX.Element,
     categoryName: string,
+    category: string
   }
 }
 
 const ServiceCategoryCard: React.FC<PropsType> = ({ item }) => {
   return (
-    <button className="group bg-white flex-center flex-col shadow-md rounded-md border border-primary-100 p-4 hover:border-primary hover:shadow-xl transition ease-in-out duration-300">
-      <span className="text-4xl text-center text-dark group-hover:text-primary group-hover:scale-125 transition ease-in-out duration-300">
-        {item?.icon}
-      </span>
-      <span className='group-hover:text-primary'>{item?.categoryName}</span>
-    </button>
+    <>
+      <Link href="/" className="group bg-white flex-center flex-col shadow-md rounded-md border border-primary-100 p-4 hover:border-primary hover:shadow-xl transition ease-in-out duration-300">
+
+        <span className="text-4xl text-center text-dark group-hover:text-primary group-hover:scale-125 transition ease-in-out duration-300">
+          {item?.icon}
+        </span>
+        <span className='group-hover:text-primary'>{item?.categoryName}</span>
+
+        <div className="w-full">
+          {
+            services.filter((filterItem, i: number) => filterItem.service_category === item.category).map((service, i: number) => {
+              return (
+                <div key={i} className="">
+
+                </div>
+              )
+            })
+          }
+        </div>
+
+      </Link>
+
+
+    </>
   );
 };
 
--- src/data/services.ts
+++ src/data/services.ts
@@ -9,9 +9,27 @@ export const services = [
     details: ""
   },
   {
+    service_banner: "/assets/images/services/ac-repear.jpg",
+    service_name: "AC Wash",
+    service_category: "ac",
+    rating: "4",
+    includedServices: [],
+    excludedServices: [],
+    details: ""
+  },
+  {
+    service_banner: "/assets/images/services/ac-repear.jpg",
+    service_name: "AC Doctor",
+    service_category: "ac",
+    rating: "4",
+    includedServices: [],
+    excludedServices: [],
+    details: ""
+  },
+  {
     service_banner: "/assets/images/services/electric.jpg",
     service_name: "Electric Servicing",
-    service_category: "ac",
+    service_category: "electric",
     rating: "4",
     includedServices: [],
     excludedServices: [],
@@ -20,7 +38,7 @@ export const services = [
   {
     service_banner: "/assets/images/services/home-cleaner.jpg",
     service_name: "Home cleaning Servicing",
-    service_category: "ac",
+    service_category: "home",
     rating: "4",
     includedServices: [],
     excludedServices: [],
@@ -29,7 +47,7 @@ export const services = [
   {
     service_banner: "/assets/images/services/plumber.jpg",
     service_name: "Plumber Servicing",
-    service_category: "ac",
+    service_category: "plumber",
     rating: "4",
     includedServices: [],
     excludedServices: [],
@@ -38,7 +56,7 @@ export const services = [
   {
     service_banner: "/assets/images/services/selon.jpg",
     service_name: "Home selone",
-    service_category: "ac",
+    service_category: "selone",
     rating: "4",
     includedServices: [],
     excludedServices: [],
